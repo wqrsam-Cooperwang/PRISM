0daccb2e4a91e1feb2745e7926dc6a64aeef64ff
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-24 22:44:38 +0800

    feat(collection): export market odds adapter

--- src/collection/__init__.py
+++ src/collection/__init__.py
@@ -2,11 +2,13 @@
 
 from src.collection.fixture import FixtureObservationAdapter
 from src.collection.interface import ObservationAdapter
+from src.collection.market import MarketOdds1X2Adapter
 from src.collection.models import SourceEnvelope
 from src.collection.runner import collect_observations
 
 __all__ = [
     "FixtureObservationAdapter",
+    "MarketOdds1X2Adapter",
     "ObservationAdapter",
     "SourceEnvelope",
     "collect_observations",
