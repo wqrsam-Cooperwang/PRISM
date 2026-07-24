37041ea1ecee627058c1bd87c8f0f0244fbe44e2
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-24 22:50:01 +0800

    feat(collection): export team strength form adapter

--- src/collection/__init__.py
+++ src/collection/__init__.py
@@ -5,11 +5,13 @@ from src.collection.interface import ObservationAdapter
 from src.collection.market import MarketOdds1X2Adapter
 from src.collection.models import SourceEnvelope
 from src.collection.runner import collect_observations
+from src.collection.team_strength_form import TeamStrengthFormAdapter
 
 __all__ = [
     "FixtureObservationAdapter",
     "MarketOdds1X2Adapter",
     "ObservationAdapter",
     "SourceEnvelope",
+    "TeamStrengthFormAdapter",
     "collect_observations",
 ]
