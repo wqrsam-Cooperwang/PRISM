7830fcaddada0bc718f06ca688484a12052b025c
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-24 22:06:51 +0800

    feat(models): export baseline prediction models

--- src/prediction/__init__.py
+++ src/prediction/__init__.py
@@ -1,6 +1,13 @@
 """Public prediction-model API for PRISM."""
 
+from src.prediction.baselines import EloProbabilityModel, MarketProbabilityModel
 from src.prediction.interface import PredictionModel
 from src.prediction.runner import run_model_suite, run_prediction_model
 
-__all__ = ["PredictionModel", "run_model_suite", "run_prediction_model"]
+__all__ = [
+    "EloProbabilityModel",
+    "MarketProbabilityModel",
+    "PredictionModel",
+    "run_model_suite",
+    "run_prediction_model",
+]
