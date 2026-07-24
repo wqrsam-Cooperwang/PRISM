c6234fa3fce469ca41609025e9e40ec3c8c87da0
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-24 13:18:14 +0800

    feat(report): export prediction report models

---
+++ src/report/__init__.py
@@ -0,0 +1,29 @@
+"""Governed read-only prediction reporting for PRISM."""
+
+from src.report.models import (
+    AdjustmentReport,
+    ConfidenceReport,
+    ConsensusReport,
+    DecisionReport,
+    EngineTraceReport,
+    EvidenceReport,
+    MatchReport,
+    PredictionReport,
+    ProvenanceReport,
+    ScorelineCandidateReport,
+    ScorelineReport,
+)
+
+__all__ = [
+    "AdjustmentReport",
+    "ConfidenceReport",
+    "ConsensusReport",
+    "DecisionReport",
+    "EngineTraceReport",
+    "EvidenceReport",
+    "MatchReport",
+    "PredictionReport",
+    "ProvenanceReport",
+    "ScorelineCandidateReport",
+    "ScorelineReport",
+]
