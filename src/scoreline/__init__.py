187c92969c6c95c4323bb19450893abc10cc9ab8
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-24 12:36:49 +0800

    feat(scoreline): add scoreline package

---
+++ src/scoreline/__init__.py
@@ -0,0 +1,6 @@
+"""Governed exact-score output for PRISM."""
+
+from src.scoreline.engine import ScorelineEngine
+from src.scoreline.models import ScorelineCandidate, ScorelineOutput
+
+__all__ = ["ScorelineCandidate", "ScorelineEngine", "ScorelineOutput"]
