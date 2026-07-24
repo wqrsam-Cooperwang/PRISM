77f40d4e4cafcb74c4761e2dbd3d162fba8db2bd
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-24 22:19:33 +0800

    feat(runtime): export end-to-end prediction path

--- src/prediction/__init__.py
+++ src/prediction/__init__.py
@@ -2,12 +2,15 @@
 
 from src.prediction.baselines import EloProbabilityModel, MarketProbabilityModel
 from src.prediction.interface import PredictionModel
+from src.prediction.path import PredictionPathResult, run_baseline_prediction_path
 from src.prediction.runner import run_model_suite, run_prediction_model
 
 __all__ = [
     "EloProbabilityModel",
     "MarketProbabilityModel",
     "PredictionModel",
+    "PredictionPathResult",
+    "run_baseline_prediction_path",
     "run_model_suite",
     "run_prediction_model",
 ]
