b38500429a0e3770633e30a4d6d4b223f94bcf23
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-25 12:46:58 +0800

    feat(collection): export collected prediction path

--- src/prediction/__init__.py
+++ src/prediction/__init__.py
@@ -1,16 +1,22 @@
 """Public prediction-model API for PRISM."""
 
 from src.prediction.baselines import EloProbabilityModel, MarketProbabilityModel
+from src.prediction.collection_path import (
+    CollectedPredictionPathResult,
+    run_collected_prediction_path,
+)
 from src.prediction.interface import PredictionModel
 from src.prediction.path import PredictionPathResult, run_baseline_prediction_path
 from src.prediction.runner import run_model_suite, run_prediction_model
 
 __all__ = [
+    "CollectedPredictionPathResult",
     "EloProbabilityModel",
     "MarketProbabilityModel",
     "PredictionModel",
     "PredictionPathResult",
     "run_baseline_prediction_path",
+    "run_collected_prediction_path",
     "run_model_suite",
     "run_prediction_model",
 ]
