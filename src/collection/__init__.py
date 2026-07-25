6e1f97ab2604a0e52a5bd1ea63a0bd4c909ffd78
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-25 12:51:01 +0800

    feat(collection): export readiness gate API

--- src/collection/__init__.py
+++ src/collection/__init__.py
@@ -5,12 +5,19 @@ from src.collection.fixture import FixtureObservationAdapter
 from src.collection.interface import ObservationAdapter
 from src.collection.market import MarketOdds1X2Adapter
 from src.collection.models import SourceEnvelope
+from src.collection.readiness import (
+    CollectionGateDecision,
+    CollectionReadinessGateResult,
+    evaluate_collection_readiness,
+)
 from src.collection.runner import collect_observations
 from src.collection.team_strength_form import TeamStrengthFormAdapter
 from src.collection.weather_lineup import WeatherLineupAdapter
 
 __all__ = [
     "AvailabilityScheduleAdapter",
+    "CollectionGateDecision",
+    "CollectionReadinessGateResult",
     "FixtureObservationAdapter",
     "MarketOdds1X2Adapter",
     "ObservationAdapter",
@@ -18,4 +25,5 @@ __all__ = [
     "TeamStrengthFormAdapter",
     "WeatherLineupAdapter",
     "collect_observations",
+    "evaluate_collection_readiness",
 ]
