7fd541980aed5109aab44f61bf72eaeaa4902898
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-26 19:28:41 +0800

    feat(acquisition): export acquired production path

--- src/acquisition/__init__.py
+++ src/acquisition/__init__.py
@@ -3,6 +3,7 @@
 from src.acquisition.fixture import FixtureProviderClient
 from src.acquisition.interface import ProviderClient
 from src.acquisition.models import ProviderFetchRequest
+from src.acquisition.production_path import run_acquired_prediction_path
 from src.acquisition.runner import ProviderAcquisitionError, acquire_source_envelopes
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "ProviderClient",
     "ProviderFetchRequest",
     "acquire_source_envelopes",
+    "run_acquired_prediction_path",
 ]
