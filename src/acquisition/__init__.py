0b3d32b073fed800fcace8260d792d7350ccd3ac
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-26 20:44:38 +0800

    feat(provider): export runtime configuration

--- src/acquisition/__init__.py
+++ src/acquisition/__init__.py
@@ -5,14 +5,20 @@ from src.acquisition.interface import ProviderClient
 from src.acquisition.models import ProviderFetchRequest
 from src.acquisition.production_path import run_acquired_prediction_path
 from src.acquisition.runner import ProviderAcquisitionError, acquire_source_envelopes
+from src.acquisition.runtime_config import (
+    OddsProviderRuntimeConfig,
+    build_the_odds_api_market_client,
+)
 from src.acquisition.the_odds_api import TheOddsApiV4MarketClient
 
 __all__ = [
     "FixtureProviderClient",
+    "OddsProviderRuntimeConfig",
     "ProviderAcquisitionError",
     "ProviderClient",
     "ProviderFetchRequest",
     "TheOddsApiV4MarketClient",
     "acquire_source_envelopes",
+    "build_the_odds_api_market_client",
     "run_acquired_prediction_path",
 ]
