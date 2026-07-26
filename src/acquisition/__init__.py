125f8a3235c853f660b4be800fc41349796eedae
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-26 19:59:13 +0800

    feat(provider): export The Odds API market client

--- src/acquisition/__init__.py
+++ src/acquisition/__init__.py
@@ -5,12 +5,14 @@ from src.acquisition.interface import ProviderClient
 from src.acquisition.models import ProviderFetchRequest
 from src.acquisition.production_path import run_acquired_prediction_path
 from src.acquisition.runner import ProviderAcquisitionError, acquire_source_envelopes
+from src.acquisition.the_odds_api import TheOddsApiV4MarketClient
 
 __all__ = [
     "FixtureProviderClient",
     "ProviderAcquisitionError",
     "ProviderClient",
     "ProviderFetchRequest",
+    "TheOddsApiV4MarketClient",
     "acquire_source_envelopes",
     "run_acquired_prediction_path",
 ]
