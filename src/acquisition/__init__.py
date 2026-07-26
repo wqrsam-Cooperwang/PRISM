7933368930edadb64706c03725343cfebb1ceb32
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-26 23:48:42 +0800

    feat(provider): export api-football runtime smoke api

--- src/acquisition/__init__.py
+++ src/acquisition/__init__.py
@@ -1,6 +1,14 @@
 """Public provider acquisition API for PRISM."""
 
 from src.acquisition.api_football import ApiFootballTeamStatisticsClient
+from src.acquisition.api_football_runtime import (
+    ApiFootballRuntimeConfig,
+    build_api_football_team_statistics_client,
+)
+from src.acquisition.api_football_smoke import (
+    LiveTeamStatisticsSmokeSummary,
+    run_live_team_statistics_smoke,
+)
 from src.acquisition.fixture import FixtureProviderClient
 from src.acquisition.interface import ProviderClient
 from src.acquisition.live_production import run_live_market_prediction_path
@@ -15,17 +23,21 @@ from src.acquisition.runtime_config import (
 from src.acquisition.the_odds_api import TheOddsApiV4MarketClient
 
 __all__ = [
+    "ApiFootballRuntimeConfig",
     "ApiFootballTeamStatisticsClient",
     "FixtureProviderClient",
     "LiveOddsSmokeSummary",
+    "LiveTeamStatisticsSmokeSummary",
     "OddsProviderRuntimeConfig",
     "ProviderAcquisitionError",
     "ProviderClient",
     "ProviderFetchRequest",
     "TheOddsApiV4MarketClient",
     "acquire_source_envelopes",
+    "build_api_football_team_statistics_client",
     "build_the_odds_api_market_client",
     "run_acquired_prediction_path",
     "run_live_market_prediction_path",
     "run_live_odds_smoke",
+    "run_live_team_statistics_smoke",
 ]
