1ea2dd775bc97a6702464422ac507310e789bb4e
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-27 04:17:33 +0800

    feat(ledger): export outcome ledger api

--- src/ledger/__init__.py
+++ src/ledger/__init__.py
@@ -2,13 +2,16 @@
 
 from src.ledger.formal import FormalPredictionResult, run_formal_acquired_prediction_path
 from src.ledger.models import LEDGER_SCHEMA_VERSION, PredictionLedgerSnapshot
+from src.ledger.outcomes import FileSystemOutcomeLedgerStore, MatchOutcome
 from src.ledger.snapshot import build_prediction_snapshot
 from src.ledger.store import FileSystemPredictionLedgerStore, PredictionLedgerStore
 
 __all__ = [
     "LEDGER_SCHEMA_VERSION",
+    "FileSystemOutcomeLedgerStore",
     "FileSystemPredictionLedgerStore",
     "FormalPredictionResult",
+    "MatchOutcome",
     "PredictionLedgerSnapshot",
     "PredictionLedgerStore",
     "build_prediction_snapshot",
