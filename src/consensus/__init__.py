3d36f4c237a4bc178b150de5ad6e488934f08da3
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-27 15:03:03 +0800

    style(v2.2): format direction calibrator export

--- src/consensus/__init__.py
+++ src/consensus/__init__.py
@@ -1,6 +1,9 @@
 """Model consensus aggregation for PRISM."""
 
-from src.consensus.direction_calibration import DirectionCalibrationOutput, DirectionCalibrator
+from src.consensus.direction_calibration import (
+    DirectionCalibrationOutput,
+    DirectionCalibrator,
+)
 from src.consensus.engine import ConsensusEngine
 
 __all__ = ["ConsensusEngine", "DirectionCalibrationOutput", "DirectionCalibrator"]
