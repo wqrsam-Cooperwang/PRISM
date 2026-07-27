87234c67f59ec948d6fb5e4a71a447a56fa8c0f1
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-27 21:09:30 +0800

    feat(regression): export governed ledger dataset loader

--- src/regression/__init__.py
+++ src/regression/__init__.py
@@ -3,6 +3,7 @@
 from src.regression.batch import BatchScorelineRegressionResult, run_batch_scoreline_regression
 from src.regression.dataset import load_scoreline_regression_dataset
 from src.regression.error_taxonomy import HistoricalErrorTaxonomy, build_historical_error_taxonomy
+from src.regression.governed_dataset import load_governed_ledger_regression_dataset
 from src.regression.importer import regression_case_from_ledgers
 from src.regression.outcome_benchmark import (
     LegacyOutcomeCase,
@@ -75,6 +76,7 @@ __all__ = [
     "evaluate_v22_promotion",
     "evaluate_v22_promotion_with_shadow",
     "full_stack_shadow_validation_passed",
+    "load_governed_ledger_regression_dataset",
     "load_legacy_outcome_cases",
     "load_scoreline_regression_dataset",
     "regression_case_from_ledgers",
