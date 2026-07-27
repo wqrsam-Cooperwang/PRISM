6aa83a86a31d560bfa46545b7a96d912da6b3fa0
wqrsam-Cooperwang <wqr.sam@gmail.com>
2026-07-27 14:38:32 +0800

    feat(regression): export legacy outcome benchmark

--- src/regression/__init__.py
+++ src/regression/__init__.py
@@ -3,6 +3,14 @@
 from src.regression.batch import BatchScorelineRegressionResult, run_batch_scoreline_regression
 from src.regression.dataset import load_scoreline_regression_dataset
 from src.regression.importer import regression_case_from_ledgers
+from src.regression.outcome_benchmark import (
+    LegacyOutcomeCase,
+    LegacyOutcomeMetrics,
+    LegacyOutcomeSummary,
+    evaluate_legacy_outcome_case,
+    load_legacy_outcome_cases,
+    summarize_legacy_outcomes,
+)
 from src.regression.scoreline import (
     ScorelineEngineMetrics,
     ScorelineRegressionCase,
@@ -14,13 +22,19 @@ from src.regression.scoreline import (
 
 __all__ = [
     "BatchScorelineRegressionResult",
+    "LegacyOutcomeCase",
+    "LegacyOutcomeMetrics",
+    "LegacyOutcomeSummary",
     "ScorelineEngineMetrics",
     "ScorelineRegressionCase",
     "ScorelineRegressionComparison",
     "ScorelineRegressionSummary",
     "compare_scoreline_case",
+    "evaluate_legacy_outcome_case",
+    "load_legacy_outcome_cases",
     "load_scoreline_regression_dataset",
     "regression_case_from_ledgers",
     "run_batch_scoreline_regression",
+    "summarize_legacy_outcomes",
     "summarize_scoreline_regression",
 ]
